5985f132e4a1a58ac39c814a343c1882e50515ca
Alef Lisboa <152299648+Elfa2004@users.noreply.github.com>
2024-11-13 08:46:30 -0300

    Add files via upload

---
+++ sqlBD/pokemon.sql
@@ -0,0 +1,14 @@
+create schema alefdejesus;
+use alefdejesus;
+create table Pokemon (
+PikachuID int,
+Tipagem varchar(255),
+Mestre varchar(255),
+Peso varchar(255),
+Altura varchar(255)
+);
+
+create table table_Pokemon (
+column1 ,
+column2 dataype
+);
