56741af40dc6dd4b36e96a1a86dbcbf6a0d98801
Alef Lisboa <152299648+Elfa2004@users.noreply.github.com>
2024-12-04 08:52:30 -0300

    Add files via upload

--- pokemon.sql
+++ pokemon.sql
@@ -9,4 +9,25 @@ email varchar (100)
 select * from estudante;
 select nome from estudante;
 insert into estudante (nome, telefone, email)
-values ('Ricardo', '9999999', 'maldivapunk@gmail.com')
+values ('Ricardo', '9999999', 'maldivapunk@gmail.com');
+drop table estudante;
+
+create schema info02;
+use info02;
+show tables;
+create table estudante(
+estudanteId int auto_increment,
+nome varchar (50) not null,
+email varchar (200) not null,
+telefone varchar (20),
+primary key(estudanteId));
+
+create table turma(
+turmaId int auto_increment,
+nome varchar (50) not null,
+primary key (turmaId)
+);
+
+select * from estudante;
+alter table estudante add turmaId int;
+alter table estudante add foreign key(turmaId) references turma(turmaId);
